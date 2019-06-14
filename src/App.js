aebfee521c216f59332f39219722320e9634cb28
name <1212@sina.com>
2019-06-14 22:51:02 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React, {Component} from 'react'
-import {HashRouter, BrowserRouter, Route, Switch} from 'react-router-dom'
-import Login from './pages/login/login.jsx'
-import Admin from  './pages/admin/admin.jsx'
+import React,{Component} from 'react'
+import {BrowserRouter,Route,Switch} from 'react-router-dom'
+import Login from './pages/login/Login.jsx'
+import Admin from './pages/admin/Admin.jsx'
 
+/* 应用根组件 */
 
+export default class App extends Component{
+    render(){
+        return (
+            <BrowserRouter>
+             <Switch>
+               <Route path="/" component={Login}/>
+               <Route path="/admin" component={Admin}/>
+             </Switch>
+            </BrowserRouter>
+        )
+    }
+}
