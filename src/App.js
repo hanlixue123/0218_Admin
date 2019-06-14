ce0327fc39a44e20114d7dc86a703a7d038adf39
name <1212@sina.com>
2019-06-14 19:04:06 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React, {Component} from 'react'
+import {HashRouter, BrowserRouter, Route, Switch} from 'react-router-dom'
+import Login from './pages/login/login.jsx'
+import Admin from  './pages/admin/admin.jsx'
+
+
